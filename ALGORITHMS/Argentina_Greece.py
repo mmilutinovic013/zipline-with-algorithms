839ae354c892e1b0ff8b2d31da8f5968f8932f1f
mmilutinovic1313 <mmilutinovic1313@gmail.com>
2015-07-02 14:45:30 +0200

    Based some of the trade off the other variables?

--- ALGORITHMS/Argentina_Greece.py
+++ ALGORITHMS/Argentina_Greece.py
@@ -36,11 +36,46 @@ def initialize(context):
     # Currently specified: Argentinian Stocks 
     #
 	context.stocks = symbols('APSA','BMA', 'CRESY', 'TEO', 'TS', 'TGS')
-	context.daycounter = 0
+context.vwap = {}
+    context.price = {}
+ 
+    # Setting our maximum position size, like previous example
+    context.max_notional = 1000000.1
+    context.min_notional = -1000000.0
+
+    # Initializing the time variables we use for logging
+    # Convert timezone to US EST to avoid confusion
+    est = pytz.timezone('EST')
+    context.d=datetime.datetime(2000, 1, 1, 0, 0, 0, tzinfo=est)
+   
 
 def handle_data(context, data):
-	# You can pass a string variable into record().
-    # Here we record the price of all the stocks in our universe.
-    for stock in data:
-      price = data[stock].price
-      record(stock, price)
+    # Initializing the position as zero at the start of each frame
+    notional=0
+    
+    # This runs through each stock.  It computes
+    # our position at the start of each frame.
+    for stock in context.stocks:
+        price = data[stock].price 
+        notional = notional + context.portfolio.positions[stock].amount * price
+        tradeday = data[stock].datetime
+        
+    # This runs through each stock again.  It finds the price and calculates
+    # the volume-weighted average price.  If the price is moving quickly, and
+    # we have not exceeded our position limits, it executes the order and
+    # updates our position.
+    for stock in context.stocks:   
+        vwap = data[stock].vwap(3)
+        price = data[stock].price  
+
+        if price < vwap * 0.995 and notional > context.min_notional:
+            order(stock,-100)
+            notional = notional - price*100
+        elif price > vwap * 1.005 and notional < context.max_notional:
+            order(stock,+100)
+            notional = notional + price*100
+
+    # If this is the first trade of the day, it logs the notional.
+    if (context.d + datetime.timedelta(days=1)) < tradeday:
+        log.debug(str(notional) + ' - notional start ' + tradeday.strftime('%m/%d/%y'))
+        context.d = tradeday
